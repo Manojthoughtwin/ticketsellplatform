41ff9b44c0bec50916f39c37fb352f070a94b31c
Saloni3097 <saloni@thoughtwin.com>
2023-03-13 16:14:44 +0530

    Profile Page

---
+++ my-app/src/components/Profile/index.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { React, useState, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+    return (
+        <>
+            <div>
+                <h1>Profile Page</h1>
+
+            </div>
+        </>
+    )
+
+
+}
